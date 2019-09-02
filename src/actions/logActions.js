01f7387c5897e761ea2f1095ddc1204dcfbd4b73
Matin Sasanpour <matin.sasan@gmail.com>
2019-09-02 21:42:20 +0430

    EditLogModal can edit now through redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG
 } from './types';
 
 export const getLogs = () => async dispatch => {
@@ -68,6 +71,45 @@ export const addLog = log => async dispatch => {
       payload: err.response.data
     });
   }
+};
+
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  };
+};
+
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  };
 };
 
 export const setLoading = () => {
--- src/components/logs/EditLogModa.jsx
+++ src/components/logs/EditLogModa.jsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import M from 'materialize-css/dist/js/materialize.min';
+import { updateLog } from '../../actions/logActions';
+import PropTypes from 'prop-types';
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
+
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message);
+      setAttention(current.attention);
+      setTech(current.tech);
+    }
+  }, [current]);
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
       return M.toast({ html: 'Please enter a message and tech' });
     }
-    console.log('submit', message, tech, attention);
+
+    const upLog = {
+      id: current.id,
+      message,
+      attention,
+      tech,
+      date: new Date()
+    };
+
+    updateLog(upLog);
+    M.toast({ html: `Log updated by ${tech}` });
     // clear fields
     setMessage('');
     setTech('');
@@ -29,9 +50,6 @@ const EditLogModal = () => {
               value={message}
               onChange={e => setMessage(e.target.value)}
             />
-            <label htmlFor='message' className='active'>
-              Log Message
-            </label>
           </div>
         </div>
         <div className='row'>
@@ -84,4 +102,16 @@ const modalStyle = {
   heigh: '75%'
 };
 
-export default EditLogModal;
+EditLogModal.prototypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  current: state.log.current
+});
+
+export default connect(
+  mapStateToProps,
+  { updateLog }
+)(EditLogModal);
